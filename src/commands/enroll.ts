ebee773eb8f7f1261f54038440700d2f09ad09ff
Ben Segal <ben@segals.org>
2025-12-20 14:22:38 -0500

    add proper enroll

--- src/commands/enroll.ts
+++ src/commands/enroll.ts
@@ -1,6 +1,8 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command } from '@sapphire/framework';
 import { InteractionContextType, MessageFlags } from 'discord.js';
+import { DialogueManager } from '../lib/dialogueManager';
+import { User } from '../types/database';
 
 @ApplyOptions<Command.Options>({
 	description: 'Enroll in new episode notifications'
@@ -17,11 +19,20 @@ export class UserCommand extends Command {
 	}
 
 	public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+		const user: User = { id: interaction.user.id, channelId: interaction.channelId };
 		await this.container.db.users.findOneAndUpdate(
 			{ id: interaction.user.id },
-			{ $set: { id: interaction.user.id, channelId: interaction.channelId } },
+			{ $set: user },
 			{ upsert: true }
 		);
+
+		interaction.channel?.messages.fetch().then(messages => messages.forEach(message => {
+			if (message.deletable) message.delete();
+		}));
+
+		const dm = await DialogueManager.create(user);
+		this.container.dialogueManagers.set(user.id, dm);
+
 		return interaction.reply({ content: 'You\'re enrolled!', flags: MessageFlags.Ephemeral });
 	}
 
